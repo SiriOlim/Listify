dce9f7f77096ba46addc91c67f44c67820cde7d0
Marlon Reyes <marlonsreyes@yahoo.com>
2022-05-30 21:05:13 -0500

    +enable swagger doc (#2)

--- TestProj/Program.cs
+++ TestProj/Program.cs
@@ -11,12 +11,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddServices();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
